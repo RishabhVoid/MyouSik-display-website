0630851dea592a5abd051f31bf019f003a46c908
Rishabh <rishabhvoid@gmail.com>
2023-07-27 11:45:50 +0530

    Removed unused CustomCursor from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import CustomCursor from "./components/CustomCursor.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <CustomCursor /> */}
     <App />
   </React.StrictMode>
 );
